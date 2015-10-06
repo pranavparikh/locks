63e60ce4caa5288ab88921244f0445669c240cc5
Maciej Adwent <MAdwent@walmartlabs.com>
2015-10-06 15:45:35 -0700

    disable keyboard listener

--- src/server.js
+++ src/server.js
@@ -8,22 +8,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var monitor = require("./monitor");
 
-process.stdin.resume(); 
-process.stdin.setEncoding("utf8"); 
-process.stdin.setRawMode(true); 
-process.stdin.on("data", function (key) { 
-  if (key === "\3") { 
-    console.log("\nShutting down server ..."); 
-    process.exit(); 
-  } else if (key === "c") {
-    console.log("\nGenerating a synthetic claim...");
-    if (monitor.claimVM()) {
-      console.log("(claim accepted)");
-    } else {
-      console.log("(claim rejected: too many claims right now)");
-    }
-  }
-});
+// process.stdin.resume(); 
+// process.stdin.setEncoding("utf8"); 
+// process.stdin.setRawMode(true); 
+// process.stdin.on("data", function (key) { 
+//   if (key === "\3") { 
+//     console.log("\nShutting down server ..."); 
+//     process.exit(); 
+//   } else if (key === "c") {
+//     console.log("\nGenerating a synthetic claim...");
+//     if (monitor.claimVM()) {
+//       console.log("(claim accepted)");
+//     } else {
+//       console.log("(claim rejected: too many claims right now)");
+//     }
+//   }
+// });
 
 app.post("/claim", function (req, res) {
   var claim = monitor.claimVM();
