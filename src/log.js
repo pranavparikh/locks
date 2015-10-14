822439a34139538e9f7933656e5c503f5cb9cf76
Maciej Adwent <MAdwent@walmartlabs.com>
2015-10-14 11:23:16 -0700

    cert/ca support

--- src/log.js
+++ src/log.js
@@ -1,15 +1,28 @@
 var request = require("request");
 var argv = require("marge").argv;
+var path = require("path");
 
 module.exports = function (ev) {
   if (argv.kibana_url) {
-    request({
+    var options = {
       method: "POST",
       json: true,
       timeout: 1000,
       body: ev,
       url: argv.kibana_url
-    }, function (error, response, body) {
+    };
+
+    if (argv.ca) {
+      var ca = fs.readFileSync(path.resolve(argv.ca));
+      options.ca = ca;
+    }
+
+    if (argv.cert) {
+      var cert = fs.readFileSync(path.resolve(argv.cert));
+      options.cert = cert;
+    }
+
+    request(options, function (error, response, body) {
       if (error) {
         console.log("logging error:", error);
       } else {
